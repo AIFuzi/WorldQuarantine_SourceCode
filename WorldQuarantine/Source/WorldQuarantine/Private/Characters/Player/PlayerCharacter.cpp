88621b1a03dd7be5fc0819b0d97ddfcb316ddbfe
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2020-09-27 02:08:26 +0300

    Create player blueprint

--- WorldQuarantine/Source/WorldQuarantine/Private/Characters/Player/PlayerCharacter.cpp
+++ WorldQuarantine/Source/WorldQuarantine/Private/Characters/Player/PlayerCharacter.cpp
@@ -1,5 +1,1 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "PlayerCharacter.h"
-
+#include "..//Public/Characters/Player/PlayerCharacter.h"
--- WorldQuarantine/Source/WorldQuarantine/Public/Characters/Player/PlayerCharacter.h
+++ WorldQuarantine/Source/WorldQuarantine/Public/Characters/Player/PlayerCharacter.h
@@ -1,14 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Characters/BaseCharacter.h"
 #include "PlayerCharacter.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class WORLDQUARANTINE_API APlayerCharacter : public ABaseCharacter
 {
