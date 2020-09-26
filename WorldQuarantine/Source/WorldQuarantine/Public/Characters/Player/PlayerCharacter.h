2aad700822e2cd1f1f542bfe3b2385d52011e509
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2020-09-27 02:04:52 +0300

    Create base character class

---
+++ WorldQuarantine/Source/WorldQuarantine/Public/Characters/Player/PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/BaseCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WORLDQUARANTINE_API APlayerCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
