8b97e360b17dbd578ed417ba094e696a7bb9189e
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2020-09-27 00:09:00 +0300

    Init

---
+++ WorldQuarantine/Source/WorldQuarantine/WorldQuarantineGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WorldQuarantineGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WORLDQUARANTINE_API AWorldQuarantineGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
